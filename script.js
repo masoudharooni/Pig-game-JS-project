0613cb4f97b3610999fe8b7ff606b23c287c89a8
masoud harooni <masoudharooni50@gmail.com>
2022-10-24 20:52:18 +0330

    Done hold score functionaliry

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
+const winnerScore = 10;
 const rollBtn = document.querySelector('.btn--roll');
+const holdBtn = document.querySelector('.btn--hold');
 const diceImageElement = document.querySelector('img.dice');
 const totalScoreEl = [
     document.getElementById('score--0'),
@@ -9,9 +11,14 @@ const currentScoreEl = [
     document.getElementById('current--0'),
     document.getElementById('current--1')
 ];
+const clearCurrentScoreNum = function () {
+    currentScoreNum[activePlayer] = 0;
+    currentScoreEl[activePlayer].textContent = currentScoreNum[activePlayer];
+}
 let currentScoreNum = [0, 0];
 let activePlayer = 0;
 const playersEl = document.querySelectorAll('.player');
+let totalScoreNum;
 const switchPlayer = function () {
     activePlayer = 1 - activePlayer;
     for (let i = 0; i < playersEl.length; i++) {
@@ -31,8 +38,19 @@ rollBtn.addEventListener('click', function () {
         currentScoreNum[activePlayer] = Number(currentScoreEl[activePlayer].textContent) + diceNumber;
         currentScoreEl[activePlayer].textContent = currentScoreNum[activePlayer];
     } else {
-        currentScoreNum[activePlayer] = 0;
-        currentScoreEl[activePlayer].textContent = currentScoreNum[activePlayer];
+        clearCurrentScoreNum();
+        switchPlayer();
+    }
+});
+
+
+holdBtn.addEventListener('click', function () {
+    totalScoreNum = Number(totalScoreEl[activePlayer].textContent);
+    if (currentScoreNum[activePlayer] === 0) {
+        alert('Roll dice!');
+    } else {
+        totalScoreEl[activePlayer].textContent = totalScoreNum + currentScoreNum[activePlayer];
+        clearCurrentScoreNum();
         switchPlayer();
     }
 });
